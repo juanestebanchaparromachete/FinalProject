764812a74912829af4ebbe1fd3bf4da710df5df9
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2017-12-10 09:15:21 -0500

    added user id and values

--- imports/ui/SmallElements/Sale.jsx
+++ imports/ui/SmallElements/Sale.jsx
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import classnames from 'classnames';
-import { Session } from 'meteor/session'
+import {Session} from 'meteor/session'
 
 
 // Task component - represents a single todo item
@@ -11,7 +11,11 @@ export default class Sale extends Component {
         super(props);
         window.scrollTo(0, 0);
         this.state = {
-            value: []
+            value: [],
+            prod: '',
+            quant:0,
+            val: 0
+
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -21,28 +25,28 @@ export default class Sale extends Component {
     handleSubmit(event) {
         event.preventDefault();
 
-        productoASumar=this.state.value;
-        challengeId=this.props.sale._id;
-        userId=Meteor.userId();
-        products=this.props.sale.products;
-        quantities=[];
-        for (i=0; i<products.length; i++)
-        {
-          if(productoASumar==products[i])
-          {
-            quantities.push(1);
-          }else{
-              quantities.push(0);
-          }
+        productoASumar = this.state.prod;
+        challengeId = this.props.sale._id;
+        userId = Meteor.userId();
+        products = this.props.sale.products;
+        quantities = [];
+        for (i = 0; i < products.length; i++) {
+            if (productoASumar == products[i]) {
+                quantities.push(this.state.quant);
+            } else {
+                quantities.push(0);
+            }
         }
-        venta = {challengeId:challengeId, userId:userId, products:products, quantities:quantities, value:350000}
+        valueff = this.state.val;
+        usernameff = Meteor.user().username;
+        venta = {challengeId: challengeId, userId: userId, products: products, quantities: quantities, value: valueff, userName:usernameff }
         Meteor.call('sales.add', venta, function (error, result) {
-          if (error) {
-            console.log(error)
-          }
-          else{
-            console.log('we good')
-          }
+            if (error) {
+                console.log(error)
+            }
+            else {S
+                console.log('we good')
+            }
         });
 
 
@@ -52,37 +56,55 @@ export default class Sale extends Component {
         this.setState({value: event.target.value});
     }
 
-  render() {
-    const taskClassName = classnames({
-      // checked: this.props.challenge.checked,
-      // private: this.props.challenge.private,
-    });
-    return (
-      <div className="col-md-12 mb-12" height="700px !important">
-        <div className="card h-200">
-          <div className="card-body">
-            <img className="card-img-top" src={this.props.sale.thumbnail} width="200px" alt="Imagen descriptiva proyecto"/>
-            <p className="card-text">{this.props.sale.description}</p>
-            <form id="contact" className="form" onSubmit={this.handleSubmit}>
-            <div className="row">
-            <select onChange={this.handleChange}>
-                {this.props.sale.products.map(function(listValue){
-                    return (
-                        <option  key = {listValue}> {listValue}</option>);
-                })}
-            </select>
+    render() {
+        const taskClassName = classnames({
+            // checked: this.props.challenge.checked,
+            // private: this.props.challenge.private,
+        });
+        return (
+            <div className="col-md-12 mb-12" height="700px !important">
+                <div className="card h-200">
+                    <div className="card-body">
+                        <img className="card-img-top" src={this.props.sale.thumbnail} width="200px"
+                             alt="Imagen descriptiva proyecto"/>
+                        <p className="card-text">{this.props.sale.description}</p>
+                        <form id="contact" className="form" onSubmit={this.handleSubmit}>
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <label >Choose product: </label>
+                                    <br/>
+                                    <select value={this.state.prod}
+                                            onChange={(event) => this.setState({prod: event.target.value})}>
+                                        {this.props.sale.products.map(function (listValue) {
+                                            return (
+                                                <option key={listValue}> {listValue}</option>);
+                                        })}
+                                    </select>
+                                </div>
+                                <div className="col-md-4">
+                                    <label >Insert quantity: </label>
+                                    <input type="text" id="uname" name="name" value={this.state.quant}
+                                           onChange={(event) => this.setState({quant: event.target.value})}/>
+                                </div>
+                                <div className="col-md-4">
+                                    <label >Insert value: </label>
+                                    <input type="text" id="uname" name="name"value={this.state.val}
+                                           onChange={(event) => this.setState({val: event.target.value})}/>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="row">
+                                <br/>
+                                <fieldset>
+                                    <button name="submit" type="submit" onSubmit={this.handleSubmit}
+                                            className="btn btn-primary moreInfoButton">Assign sale
+                                    </button>
+                                </fieldset>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <br/>
-            <div className="row">
-              <br/>
-              <fieldset>
-              <button name="submit" type="submit" onSubmit={this.handleSubmit} className="btn btn-primary moreInfoButton">Asignar venta</button>
-              </fieldset>
-            </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
