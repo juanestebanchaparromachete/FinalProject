363862e73e5b4f1d43fd139256b311cef64880d0
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2017-12-11 07:49:54 -0500

    descripcion challenges

--- imports/ui/MainElements/ChallengesList.jsx
+++ imports/ui/MainElements/ChallengesList.jsx
@@ -3,7 +3,7 @@ import {createContainer} from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import Navbar from '../SmallElements/Navbar.jsx'
-import { Challenges } from "../../api/challenges.jsx";
+import {Challenges} from "../../api/challenges.jsx";
 import Challenge from "../SmallElements/Challenge.jsx";
 import LandingNavbar from "../SmallElements/LandingNavbar";
 import {Redirect} from 'react-router';
@@ -11,76 +11,76 @@ import {Redirect} from 'react-router';
 // ProjectsView component - represents the whole app
 class ChallengesList extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hideCompleted: false
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            hideCompleted: false
+        };
+    }
 
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
-  }
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
 
-  renderProjects() {
-    let filteredChallenges = this.props.challenges;
-    // console.log(filteredChallenges);
-    return filteredChallenges.map((challenge) => {
-      if (challenge._id)
+    renderProjects() {
+        let filteredChallenges = this.props.challenges;
+        // console.log(filteredChallenges);
+        return filteredChallenges.map((challenge) => {
+            if (challenge._id)
+                return (
+                    <Challenge
+                        key={challenge._id}
+                        challenge={challenge}
+                    />
+                );
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Meteor.call('tasks.insert', text);
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    render() {
         return (
-          <Challenge
-            key={challenge._id}
-            challenge={challenge}
-          />
+            <div>
+                { Meteor.userId() ? null : <Redirect push to="/main"/> }
+                <LandingNavbar/>
+                <div className="row my-4">
+                    <div className="container">
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <h1>Active challenges</h1>
+
+                    </div>
+                </div>
+                <div className="container">
+                    <hr/>
+                    <p>Here you can find all the active challenges of your brand. Check out any challenge and find out the current ranking of sellers.</p>
+
+                    <div className="row">
+                        {this.renderProjects()}
+                    </div>
+                </div>
+            </div>
         );
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
-    Meteor.call('tasks.insert', text);
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
-
-  render() {
-    return (
-      <div>
-        { Meteor.userId() ? null : <Redirect push to="/main"/> }
-        <LandingNavbar/>
-        <div className="row my-4">
-          <div className="container">
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <h1>Active challenges</h1>
-
-          </div>
-
-        </div>
-        <div className="container">
-          <hr/>
-
-          <div className="row">
-          {this.renderProjects()}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    }
 }
 
 export default createContainer(() => {
-  Meteor.subscribe('challenges', Meteor.userId());
-  return {
-    challenges: Challenges.find({}, {sort: {createdAt: -1}}).fetch(),
-    currentUser: Meteor.user(),
-  };
+    Meteor.subscribe('challenges', Meteor.userId());
+    return {
+        challenges: Challenges.find({}, {sort: {createdAt: -1}}).fetch(),
+        currentUser: Meteor.user(),
+    };
 }, ChallengesList);
